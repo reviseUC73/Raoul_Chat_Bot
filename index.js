6318bb0bff8f8e4130fd5faf0824dedf715cb129
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-01-25 22:08:56 +0700

    The bot is ready to use

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js'); // import from Discord client
+const client = new Client({ intents: 
+  [GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent
+  ] });
+const dotenv  = require('dotenv');
+dotenv.config();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+// command in Discord
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
