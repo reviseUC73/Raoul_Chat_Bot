22798b660e0bff7e589e5444727d95a0206edaef
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-01-27 12:02:14 +0700

    #1 have bug when gbt long ans

--- index.js
+++ index.js
@@ -28,21 +28,22 @@ client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "ping") {
-    try {
-      const completion = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: "Hello world",
-      });
-      console.log(completion.data.choices[0].text);
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
-      }
-    }
-    await interaction.reply("Pong!");
+    // try {
+    //   const completion = await openai.createCompletion({
+    //     model: "text-davinci-002",
+    //     prompt: "Hello world",
+    //   });
+    //   console.log(completion.data.choices[0].text);
+    // } catch (error) {
+    //   if (error.response) {
+    //     console.log(error.response.status);
+    //     console.log(error.response.data);
+    //   } else {
+    //     console.log(error.message);
+    //   }
+    // }
+    await interaction.reply(`A domain model is a visual representation of a system’s objects, their attributes and relationships, which helps to identify the objects, how they are related to each other, and what services they provide. It is used to design the architecture of a software system, and is usually created in the form of a diagram.  A domain model is created to help the designer think through a complex application and identify all the different objects that must be taken into account when developing the app.
+    Unknown interaction`);
   }
   //   // get message your in put
   if (interaction.commandName === "chat_gbt") {
@@ -51,51 +52,53 @@ client.on("interactionCreate", async (interaction) => {
 
     // console.log(typeof text_);
     // console.log( text_);
-    try {
-      const text_ = interaction.options.getString("message");
+    // try {
+    const text_ = interaction.options.getString("message");
 
-      const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: text_,
-        max_tokens: 2048,
-        temperature: 0.9,
-        
-      });
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text_,
+      max_tokens: 2048,
+      temperature: 0.5,
+    });
+    try {
       const response = completion.data.choices[0].text;
-      // console.log(response);
-      console.log(typeof response);
       await interaction.reply(response);
-      
     } catch (error) {
-      if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
-      }
+      console.error(error);
+      await interaction.reply(
+        "Sorry, there was an error processing your request."
+      );
     }
 
-    // try {
-    //   const completion = await openai.Completion.create({
-    //     prompt: text_,
-    //     engine: "text-davinci-003",
-    //     maxTokens: 100,
-    //     temperature: 0.9,
-    //   });
-    //   const response = completion.data.choices[0].text;
-    //   //   // send the response from the API to the Discord channel
-    //   // await interaction.reply(response.data);`
-    //   // console.log(response);
-    //   await interaction.reply(response);
-    //   // console.log(response.data.choices[0].text);
-    //   // await interaction.reply(response.data.choices[0].text);
-      
     // } catch (error) {
-    //   console.error(error);
-      
-    // }
-  
+    //   if (error.response) {
+    //     console.log(error.response.status);
+    //     console.log(error.response.data);
+    //   } else {
+    //     console.log(error.message);
+    //   }
   }
+
+  // try {
+  //   const completion = await openai.Completion.create({
+  //     prompt: text_,
+  //     engine: "text-davinci-003",
+  //     maxTokens: 100,
+  //     temperature: 0.9,
+  //   });
+  //   const response = completion.data.choices[0].text;
+  //   //   // send the response from the API to the Discord channel
+  //   // await interaction.reply(response.data);`
+  //   // console.log(response);
+  //   await interaction.reply(response);
+  //   // console.log(response.data.choices[0].text);
+  //   // await interaction.reply(response.data.choices[0].text);
+
+  // } catch (error) {
+  //   console.error(error);
+
+  // }
 });
 
 // login to Discord with your app's token
